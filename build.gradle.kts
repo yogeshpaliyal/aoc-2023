1c58b93790ccd80b60dbd1a108ebb2aa892fbdb1
Yogesh Choudhary Paliyal <yogeshpaliyal.foss@gmail.com>
2023-12-03 18:33:05 +0530

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+}
+
+sourceSets {
+    main {
+        kotlin.srcDir("src")
+    }
+}
+
+tasks {
+    wrapper {
+        gradleVersion = "8.5"
+    }
+}
